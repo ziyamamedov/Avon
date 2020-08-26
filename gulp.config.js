d599cf830dd09a6756f0316985d28217f4b0df97
Ziya Mamedov <ziya_90@mail.ru>
2020-08-26 14:10:43 +0300

    added GULP

---
+++ gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  SRC_PATH : './src',
+  DIST_PATH: './dist',
+  STYLE_LIBS: [
+    './node_modules/normalize.css/normalize.css'
+  ],
+  JS_LIBS: [
+    './node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-touchswipe/jquery.touchSwipe.js'
+  ]
+
+}
---
+++ src/scripts/components/hamburger.js
@@ -0,0 +1,10 @@
+const hamBtn = document.querySelector('#hamBtn');
+const sidebar = document.querySelector('.sidebar');
+const clsBtn = document.querySelector('#hamClsBtn')
+
+hamBtn.addEventListener('click', () => {
+  sidebar.classList.add('sidebar--active');
+})
+clsBtn.addEventListener('click', () => {
+  sidebar.classList.remove('sidebar--active');
+})
