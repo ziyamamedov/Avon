469feddae9dfa1940586248121084cd6a47af6a2
Ziya Mamedov <ziya_90@mail.ru>
2021-03-20 13:49:54 +0300

    Started easy-start page

--- src/php/reg-form.php
+++ src/php/reg-form.php
@@ -46,7 +46,7 @@
   $mail->Host = 'smtp.mail.ru';  												// Specify main and backup SMTP servers
   $mail->SMTPAuth = true;                               // Enable SMTP authentication
   $mail->Username = 'avon-cat@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-  $mail->Password = ''; // Ваш пароль от почты с которой будут отправляться письма
+  $mail->Password = 'Bogoroditsk2021'; // Ваш пароль от почты с которой будут отправляться письма
   $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
   $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
   
